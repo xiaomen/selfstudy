7c92566449084aeeeaed500dcf301921c0450590
luoruici <luoruici@gmail.com>
2012-03-05 17:16:04 +0800

    delete unused files
    --HG--
    branch : refactor_by_flask

--- app/models/university.py
+++
@@ -1,7 +0,0 @@
-import web
-from config import db
-
-def get_university_by_no(no):
-    return web.listget(
-            db.select('universities', vars=dict(no=no),
-                where='short_name=$no'), 0)
--- config.py
+++
@@ -1,1 +0,0 @@
-DATABASE_URI = 'mysql://root:Pa$$w0rd@localhost:3306/selfstudy'
