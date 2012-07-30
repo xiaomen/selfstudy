e1f302849c40a2f147bd3d258dc77295a64a00ef
CMGS <ilskdw@mspil.edu.cn>
2012-07-30 16:19:37 +0800

    dump mysql schema

---
+++ databases/schema.sql
@@ -0,0 +1,77 @@
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `selfstudy` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `selfstudy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `buildings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `university_id` int(11) DEFAULT NULL,
+  `campus_id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `no` varchar(20) DEFAULT NULL,
+  `enabled` tinyint(1) DEFAULT NULL,
+  `seq` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `university_id` (`university_id`),
+  KEY `campus_id` (`campus_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `campuses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `university_id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `no` varchar(20) DEFAULT NULL,
+  `seq` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `university_id` (`university_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `classrooms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `building_id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `no` varchar(20) DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  `seq` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `building_id` (`building_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=388 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `occupies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `classroom_id` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `occupies` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `classroom_id` (`classroom_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14239 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `periods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `university_id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `start` int(11) DEFAULT NULL,
+  `end` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `university_id` (`university_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `universities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `no` varchar(20) DEFAULT NULL,
+  `class_quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
